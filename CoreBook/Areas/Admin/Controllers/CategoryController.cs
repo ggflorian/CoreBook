6031ed6ebd28d09286ada064d01836b1c2d51be7
Florian Florian <ggflorian@gmx.com>
2020-12-04 22:57:45 +0200

    201204-2

---
+++ CoreBook/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using CoreBook.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBook.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = allObj });
+        }
+
+        #endregion
+    }
+}
